f5a3f4dfd1561686f313608acebf08c871a94c8f
jftarog <jftarog@student.apc.edu.ph>
2023-01-13 23:15:38 +0800

    Javascript Lesson - For Of

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ function onClick() {
     document.getElementsByClassName("button")[1].style.visibility = "visible";
     // --- Was testing some code, I am leaving this here as a reference for the future: ---
     let flipElement = document.getElementsByClassName("flip");
-    for (var i = 0; i< flipElement.length; ++i){
-        flipElement[i].style.display = "none";
+    // for (let i = 0; i< flipElement.length; ++i){
+    //     flipElement[i].style.display = "none";
+    // };
+    for (let i of flipElement){
+        i.style.display = "none";
     };
 };
 
 function testFunction() {
     let flipElement = document.getElementsByClassName("flip");
-    for(var i = 0; i < flipElement.length; ++i) {
-        console.log(flipElement[i])
+    for(let i of flipElement) {
+        console.log(i);
     };
 };
 
