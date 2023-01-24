acf333dcbebad35c9956b613375a948ee47a854d
jftarog <jftarog@student.apc.edu.ph>
2023-01-24 21:06:07 +0800

    Javascript Lesson 15.1 - Errors, Try Catch

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ function onClick() {
     // document.getElementsByClassName("flip")[0].style.display = "none";
     document.getElementsByClassName("button")[1].style.visibility = "visible";
     // --- Was testing some code, I am leaving this here as a reference for the future: ---
-    let flipElement = document.getElementsByClassName("flip");
     // for (let i = 0; i< flipElement.length; ++i){
     //     flipElement[i].style.display = "none";
     // };
-    for (let i of flipElement){
-        i.style.display = "none";
-    };
+    let flipElement = document.getElementsByClassName("flip");
+    for (let i of flipElement) {
+        try {
+            i.style.display = "none";
+        } catch (err) {
+            console.log("This is from a try-catch");
+            console.log(i);
+        };
+    }; 
 };
 
 function testFunction() {
@@ -19,7 +24,6 @@ function testFunction() {
         console.log(i);
     };
 };
-
 
 function displayDate(someID) {
     document.getElementById(someID).innerHTML = Date();
