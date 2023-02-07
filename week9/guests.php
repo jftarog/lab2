b0a21afc042c0fdaa48b3b8b20b717dced39895c
James Christian Tarog <jftarog@student.apc.edu.ph>
2023-02-07 15:46:34 +0800

    Update guests.php

--- week9/guests.php
+++ week9/guests.php
@@ -28,13 +28,20 @@
     die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $sql = "SELECT id, name, email, website, comment, gender FROM jftarog_myguests";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo 
+            "id: " . $row["id"] .
+            " - Name: " . $row["name"] . 
+            " - Email: " . $row["email"] . 
+            " - Website: ". $row["website"] .
+            " - Comment: ". $row["comment"] .
+            " - Gender: ". $row["gender"] .
+            "<br>";
     }
     } else {
     echo "0 results";
