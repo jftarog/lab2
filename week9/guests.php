2a6e21a151ec5a03003f53dea6febc99216d33f0
James Christian Tarog <jftarog@student.apc.edu.ph>
2023-02-07 14:19:02 +0800

    added guests.php to navbar

--- week9/guests.php
+++ week9/guests.php
@@ -1,26 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JC's Page</title>
+    <link rel="icon" type="image/x-icon" href="../images/aonahara.ico">
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/script.js"></script>
+</head>
+<body>
+    <?php include('navigation.php') ?>
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webprogss211db";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "webprogss211db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $conn->query($sql);
+    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+    } else {
+    echo "0 results";
+    }
+    $conn->close();
 ?> 
+
+</body>
+</html>
--- week9/navigation.php
+++ week9/navigation.php
@@ -3,4 +3,5 @@
    <li><a href="lessons.php" <?php if($_SERVER['PHP_SELF'] == "/lab2/week9/lessons.php") { echo 'class="active"'; }; ?>>W3Schools Lessons</a></li>
    <li><a href="resources.php" <?php if($_SERVER['PHP_SELF'] == "/lab2/week9/resources.php") { echo 'class="active"'; }; ?>>Resources</a></li>
    <li><a href="validation_complete.php" <?php if($_SERVER['PHP_SELF'] == "/lab2/week9/validation_complete.php") { echo 'class="active"'; }; ?>>Forms</a></li>
+   <li><a href="guests.php" <?php if($_SERVER['PHP_SELF'] == "/lab2/week9/guests.php") { echo 'class="active"'; }; ?>>Guests</a></li>
 </ul></div>
